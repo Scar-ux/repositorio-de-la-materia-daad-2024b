d046dcfef2e88e122040e64782560fe7bc7451ae
Scar-ux <fs3076355@gmail.com>
2024-11-08 16:56:04 +0000

    S03S04 Componente PlanPicker

---
+++ src/components/PlanPicker.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {ref} from 'vue'; 
+import planItem from './components/plan-item.vue';
+const plans = ref(['El cafetero','El chelero','El intelectual']);
+</script>
+
+
+<template> 
+   <div class="plans">
+      <planItem v-for=" plan in plans" :key="plan" :name="plan"></planItem>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
