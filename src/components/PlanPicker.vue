762539b0460ab2d539564373f72d33ad0862bb02
Scar-ux <fs3076355@gmail.com>
2024-11-13 03:22:28 +0000

    Plans

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,13 +1,19 @@
 <script setup>
-import {ref} from 'vue'; 
-import planItem from './components/plan-item.vue';
-const plans = ref(['El cafetero','El chelero','El intelectual']);
+  import PlanPickerItem from '.PlanPickerItem.vue';
+  import { ref } from 'vue';
+  const plans = ref(['El cafetero', 'El chelero', 'El intelectual']);
+  const selectPlan = (name) => {
+    console.log(name);
+  };
 </script>
 
-
-<template> 
-   <div class="plans">
-      <planItem v-for=" plan in plans" :key="plan" :name="plan"></planItem>
+<template>
+    <div class="plans">
+      <PlanPickerItem 
+      v-for="plan in plans" 
+      :key="plan"
+      :name="plan"
+      @select="(name)=>{ console.log(name) }" />
     </div>
 </template>
 
--- src/components/PlanPickerItem.vue
+++ src/components/PlanPickerItem.vue
@@ -1,22 +1,39 @@
 <template>
-  <div class="plan">
+  <div @click="selectPlan" class="plan">
     <div class="description">
-      <span class="title"> {{ name }} </span>
+      <span class="title">
+        {{ name }} {{ selected ? '🌟' : '' }}
+      </span>
     </div>
   </div>
 </template>
 
 <script setup>
-//usando un macro para 
-//definir las props 
-//defineProps(['name','planIcon']); 
-defineProps({
+//import ref function
+import { ref } from 'vue';
+
+//Usando un macro para
+//definir las props
+//defineProps(['name', 'planIcon']);
+const props = defineProps({
   name: {
     type: String,
     required: true
   }
-}); 
-
+});
+//Define event
+const emit = defineEmits(['select']);
+//Variable que permite recordar
+//si el plan a sido seleccionado 
+const selected = ref(false);
+//Funcion que permite cambiar el estado de la variable
+//selected
+const selectPlan = () => {
+selected.value = true;
+//Emitir el evento de que el 
+//plan ha sido seleccionado
+emit('select', props.name);
+};
 </script>
 
 <style scoped>
