82bef9caa31bf911284c5b7b076d1bce1b31f46d
Scar-ux <fs3076355@gmail.com>
2024-11-20 18:50:49 +0000

    S03S06 Hooks

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,6 +1,6 @@
 <script setup>
   import PlanPickerItem from './PlanPickerItem.vue';
-  import { ref } from 'vue';
+  import { ref, onMounted } from 'vue';
   //Creando una referencia reactiva para almacenar el plan deseado 
   const selectedPlan=ref(null)
   const plans = ref(['El cafetero', 'El chelero', 'El intelectual']);
@@ -8,7 +8,16 @@
     //Almacenar el plan seleccionado
     selectedPlan.value =name; 
   };
-</script>
+
+  //INTENTANDO OBTENER LA REFERENCIA AL ELEMENTO .PLANS
+  console.log("Termina de ejecutar el script setup"); 
+  //console.log (document.querySelector('.plans')); 
+  //registrando el hook mounted 
+  onMounted(() => {
+    console.log("El componente PlanPicker se ha montado");
+    console.log (document.querySelector('.plans')); 
+  }); 
+    </script>
 
 <template>
     <div class="plans">
